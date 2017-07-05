1ce37c255ccbd2c57924dc187d9c1a6a26e2a11c
zhuhongqiang <zhuhongqiang@soyoung.com>
2017-07-05 18:59:04 +0800

    新建一些目录

--- src/main/java/com/web/zhq/conrorller/loginController.java
+++ src/main/java/com/web/zhq/conrorller/loginController.java
@@ -1,11 +1,13 @@
 package com.web.zhq.conrorller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author : Created by zhq
  * @since : 2017/7/5
  */
+@Controller
 @RequestMapping(value = "/user")
 public class loginController {
 
