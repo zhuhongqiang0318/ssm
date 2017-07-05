dba64f4923837a45e501a9021fbcbafe74438a4f
zhuhongqiang <zhuhongqiang@soyoung.com>
2017-07-05 15:18:18 +0800

    测试

---
+++ src/main/java/userPo.java
@@ -0,0 +1,33 @@
+/**
+ * @author : Created by zhq
+ * @since : 2017/6/30
+ */
+public class userPo  {
+    private int uid;
+    private String uname;
+    private String mobile;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
