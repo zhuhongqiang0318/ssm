e45deeee6f29ecaf70e9722dbc3990bcb015d6c7
zhuhongqiang <zhuhongqiang@soyoung.com>
2017-07-05 18:58:03 +0800

    新增查询接口

--- src/main/java/com/web/zhq/conrorller/loginController.java
+++ src/main/java/com/web/zhq/conrorller/loginController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author : Created by zhq
  * @since : 2017/7/5
  */
-@RequestMapping("user")
+@RequestMapping(value = "/user")
 public class loginController {
 
-    @RequestMapping("getUser")
+    @RequestMapping(value = "/getUser")
     public String getUser(){
 
      return "/index";
